37dc7f44159c3f41eda1aebb5103a9b6a7a2d89a
DJay <40359449+dhanan77@users.noreply.github.com>
2023-01-16 17:37:22 +0530

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -1,15 +1,15 @@
 provider "oci" {
- tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaasg7dmeaivai6jirpcbl23ms3z4aohftxghilvru5b2eceujefw6a"
- user_ocid="ocid1.user.oc1..aaaaaaaa6u3e5g6fw367d6zgve6wxpvwfnyyj47cx7vkyhzmq5eolnf3k2wq"
- fingerprint="7e:88:af:cf:71:f1:f4:2e:e3:58:6d:21:b5:16:63:e1"
- private_key_path="~/.oci/apiprivatekey.pem"
+ tenancy_ocid="a"
+ user_ocid=""
+ fingerprint=
+ private_key_path=
  region="eu-frankfurt-1"
 }
 
 
 data "oci_identity_availability_domains" "test_availability_domains" {
     #Required
-    compartment_id ="ocid1.tenancy.oc1..aaaaaaaasg7dmeaivai6jirpcbl23ms3z4aohftxghilvru5b2eceujefw6a"
+    compartment_id =""
 }
 
 
